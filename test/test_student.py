74f5edfd8a7157aaf3e7ee186d1818edfb3b9972
ComputerUser158 <oneringraw@gmail.com>
2020-11-05 17:35:16 -0600

    topic 3 second commit: 2 tests pass

--- test/test_student.py
+++ test/test_student.py
@@ -4,7 +4,7 @@ from class_definitions import student as t
 
 class MyTestCase(unittest.TestCase):
 	def setUp(self):
-		self.student = t.Student('Collins', 'Rawley', 'Programming', 4.0)
+		self.student = t.Student('Collins', 'Rawley', 'Programming')
 
 	def tearDown(self):
 		del self.student
@@ -13,13 +13,16 @@ class MyTestCase(unittest.TestCase):
 		self.assertEqual(self.student.first_name, 'Rawley')
 		self.assertEqual(self.student.last_name, 'Collins')
 		self.assertEqual(self.student.major, 'Programming')
-		self.assertEqual(self.student.gpa, 4.0)
+		self.assertEqual(self.student.gpa, 0.0)
 
+	def test_object_created_all_attributes(self):
+		student = t.Student('Collins', 'David', 'Programming', 4.0)
+		assert student.first_name == 'David'
+		assert student.last_name == 'Collins'
+		assert student.major == 'Programming'
+		assert student.gpa == 4.0
 
 """
-	def test_something(self):
-		self.assertEqual(True, False)
-
 	def test_something(self):
 		self.assertEqual(True, False)
 
